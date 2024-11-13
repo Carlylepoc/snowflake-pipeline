edb8b01dd94008b70ec02c982c00f7f7b899703e
Carlylepoc <carlylepoc@gmail.com>
2024-11-13 11:23:25 +0530

    Create create_task.sql

---
+++ create_task.sql
@@ -0,0 +1,9 @@
+-- create_task.sql
+
+CREATE OR REPLACE TASK onboarding_data_transformation
+WAREHOUSE = my_warehouse
+SCHEDULE = 'USING CRON 0 0 * * * UTC'
+AS
+-- Call your transformation script here or embed the SQL code
+-- In this case, we are running the data transformation SQL
+SELECT * FROM onboarding_schema.transformed_investors;
