b2fba94d9e1dcf30aa7296177f6f2c4572db4d73
Carlylepoc <carlylepoc@gmail.com>
2024-11-13 11:30:08 +0530

    Update create_task.sql

--- create_task.sql
+++ create_task.sql
@@ -1,7 +1,7 @@
 -- create_task.sql
 
 CREATE OR REPLACE TASK onboarding_data_transformation
-WAREHOUSE = my_warehouse
+WAREHOUSE = COMPUTE_WAREHOUSE
 SCHEDULE = 'USING CRON 0 0 * * * UTC'
 AS
 -- Call your transformation script here or embed the SQL code
